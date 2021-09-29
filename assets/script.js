e45c69f7df15a258812702234d43299f4fdbb5dd
jbarker <jackson.barker@du.edu>
2021-09-28 20:19:51 -0600

    html and js changes to make color changes work

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,46 @@
+
 var currentDay = moment().format("ddd MMM Do, YYYY");
-$('#currentDay').text(currentDay);
+$("#currentDay").text(currentDay);
+
+$(document).ready(function () {
+  $(".saveBtn").on("click", function () {
+    var task = $(this).siblings(".description").val();
+    var hour = $(this).parent().attr("id");
+    localStorage.setItem(hour, task);
+  });
+});
+
+$("#08 .description").val(localStorage.getItem("08"));
+$("#09 .description").val(localStorage.getItem("09"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+
+var timeColor = function() {
+  var currentTime = moment().format('HH');
+
+  var timeBlockEl = $(".time-block");
+
+  for (var i = 0; i < timeBlockEl.length; i++) {
+    var timeID = timeBlockEl[i].id;
+    var changedID = document.getElementById(timeBlockEl[i].id);
+    $(timeBlockEl).removeClass(".past, .present, .future");
+    if (timeID < currentTime) {
+      $(changedID).addClass("past");
+    } else if (timeID > currentTime) {
+      $(changedID).addClass("future");
+    } else {
+      $(changedID).addClass("present");
+    }
+  }
+}
+
+timeColor();
+
+setInterval(timeColor(), (1000 * 60) *5);
+
 
